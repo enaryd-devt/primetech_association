f909e19b2faf81a964a71d45d68cb4e391ad1201
Dirane Ngousse <enaryd123@gmail.com>
2026-07-19 13:34:40 +0100

    fix: rattacher les attributions à la session

--- wizard/__init__.py
+++ wizard/__init__.py
@@ -16,3 +16,4 @@ from . import association_meeting_collection_surplus_wizard
 from . import subscription_cycle_close_wizard
 from . import subscription_payment_wizard
 from . import meeting_subscription_cycle_start_wizard
+from . import member_account_subscription_payment_wizard
