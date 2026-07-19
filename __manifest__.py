9ad5fc99dd07083506ee44912ebbac49431f8de8
Dirane Ngousse <enaryd123@gmail.com>
2026-07-19 16:48:40 +0100

    fix: intégrer les paramètres association aux réglages généraux

--- __manifest__.py
+++ __manifest__.py
@@ -105,15 +105,18 @@ Gestion financière
         "views/member_wallet_transaction_views.xml",
         "views/subscription_cycle_close_wizard_views.xml",
         "views/meeting_subscription_cycle_start_wizard_views.xml",
-
-        # Configuration (chargée avant les menus qui la référencent)
-        "views/configuration_views.xml",
+        "views/member_account_subscription_payment_wizard_views.xml",
 
         # =====================================================
         # MENUS
         # =====================================================
 
         "views/menu_views.xml",
+
+        # The configuration menu is a child of menu_association_root, which
+        # is defined above.  Load it after the root menu to support a clean
+        # first installation as well as module upgrades.
+        "views/configuration_views.xml",
 
     ],
 
